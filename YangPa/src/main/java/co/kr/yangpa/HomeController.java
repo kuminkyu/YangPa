662751485dd8f939d1f8e91fe780ff44f2af657e
kuminkyu <kuminkyu93@naver.com>
2020-10-21 15:00:10 +0900

    빈파일 삭제

--- YangPa/src/main/java/co/kr/yangpa/HomeController.java
+++ YangPa/src/main/java/co/kr/yangpa/HomeController.java
@@ -1,20 +1,11 @@
 package co.kr.yangpa;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
 @Controller
 public class HomeController {
-	
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
@@ -22,15 +13,4 @@ public class HomeController {
 		return "main";
 	}
 	
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String login() {
-		
-		return "user/login";
-	}
-	
-	@RequestMapping(value = "/find", method = RequestMethod.GET)
-	public String find1() {
-		
-		return "user/find";
-	}
 }
